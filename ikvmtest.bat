9c9074160ba213de6c768a22fac0b4a7175feb11
Jessie Lesbian <jessielesbian@protonmail.com>
2021-03-13 15:09:14 +0700

    FIX Auto Testing Not Working

--- ikvmtest.bat
+++ ikvmtest.bat
@@ -5,6 +5,7 @@ built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1
 set TEST_RESULT=%ERRORLEVEL%
 getUnixTime.bat
 set TEST_END=%ERRORLEVEL%
+rmdir scratch
 %TEST_RESULT% EQU 0 goto pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Failed
 goto end
