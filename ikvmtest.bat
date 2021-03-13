2d12ef9402504a256f621f6046e3b84d839e3c96
Jessie Lesbian <jessielesbian@protonmail.com>
2021-03-13 14:57:08 +0700

    Automated Testing

---
+++ ikvmtest.bat
@@ -0,0 +1,14 @@
+@echo off
+getUnixTime.bat
+set TEST_BEGIN=%ERRORLEVEL%
+built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1
+set TEST_RESULT=%ERRORLEVEL%
+getUnixTime.bat
+set TEST_END=%ERRORLEVEL%
+%TEST_RESULT% EQU 0 goto pass
+appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Failed
+goto end
+:pass
+appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Passed
+:end
+exit /b 0
