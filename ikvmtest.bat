8be4e827a03e410e01ce55e03f7f3a03c787476a
Jessie Lesbian <jessielesbian@protonmail.com>
2021-03-13 16:07:05 +0700

    FIX rmdir issue again!

--- ikvmtest.bat
+++ ikvmtest.bat
@@ -1,7 +1,7 @@
 @echo off
 built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1
 set TEST_RESULT=%ERRORLEVEL%
-rmdir /s scratch
+rmdir /s /q scratch
 if %TEST_RESULT% EQU 0 goto pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Failed
 goto end
