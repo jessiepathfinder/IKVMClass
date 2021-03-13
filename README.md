966de38ef15143699a7d3eeaab96dffecbc6b693
Jessie Lesbian <jessielesbian@protonmail.com>
2021-03-13 16:33:36 +0700

    Hopefully it works properly

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # IKVMClass
 [![Build status](https://ci.appveyor.com/api/projects/status/hnxdqxub38rud22i?svg=true)](https://ci.appveyor.com/project/jessielesbian/ikvmclass)
-
+PASSED
 IKVMClass is an OpenJDK fork built for IKVM.NET. Contains some work from Jeroen Frijters, Windward Studios, and ikvm-revived as well.
 ## Build
 You need to add [this ikvmstub file](https://mega.nz/file/cqARyYSR#011dw2hXY-eHVU2-I8-3e9Yl39kB2FWnkp4_U4rLFMM) to your boot class path for the build, cd into /OpenJDK and ````javac -g:none -sourcepath sourcepath282 -bootclasspath ikvmstubs.jar````!
--- ikvmtest.bat
+++ ikvmtest.bat
@@ -1,11 +1,14 @@
 @echo off
-built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1
-set TEST_RESULT=%ERRORLEVEL%
+built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1>ikvmtest.txt
 rmdir /s /q scratch
+set TEST_RESULT=1
+FOR /F "tokens=* delims=" %%x in (ikvmtest.txt) DO call chktestpassing.bat "%%x"
 if %TEST_RESULT% EQU 0 goto pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Failed
 goto end
 :pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Passed
+del /f /q TEST_PASSING
+del /f /q ikvmtest.txt
 :end
 set ERRORLEVEL=0
