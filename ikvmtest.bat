5934badbe3bcee44ef6e69bd3875e852a4780627
Jessie Lesbian <jessielesbian@protonmail.com>
2021-03-14 07:07:07 +0700

    Last BugFuck of the day

--- ikvmtest.bat
+++ ikvmtest.bat
@@ -1,7 +1,6 @@
 @echo off
 built-ikvm\bin\ikvm.exe -Xextremeoptimize -jar dacapo-9.12-MR1-bach.jar %1>ikvmtest.txt
 rmdir /s /q scratch
-set TEST_RESULT=1
 FOR /F "tokens=* delims=" %%x in (ikvmtest.txt) DO call chktestpassing.bat "%%x"
 if exist TEST_PASSING goto pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Failed
@@ -9,6 +8,6 @@ goto end
 :pass
 appveyor AddTest -Name %1 -Framework IKVMTest -FileName built-ikvm\bin\ikvm.exe -Outcome Passed
 del /f /q TEST_PASSING
-del /f /q ikvmtest.txt
 :end
 set ERRORLEVEL=0
+del /f /q ikvmtest.txt
