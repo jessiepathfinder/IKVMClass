3584a3058e2b0d23bcee48c8b124b32db8fbe18e
Jessie Lesbian <jessielesbian@protonmail.com>
2021-07-28 15:46:24 +0700

    Update TwoStacksPlainDatagramSocketImpl.java

--- openjdk/java/net/TwoStacksPlainDatagramSocketImpl.java
+++ openjdk/java/net/TwoStacksPlainDatagramSocketImpl.java
@@ -1735,25 +1735,29 @@ class TwoStacksPlainDatagramSocketImpl extends AbstractPlainDatagramSocketImpl
 		}
     }
 	
-	@Override int dataAvailable(){
-		//A pretty bad implementation
-		int peek = 0;
-		synchronized(anticlosinglock){
-			if (connectedAddress == null) {
-				
-			} else if (isClosed()) {
-				
-			} else{
-				try{
-					peek = peek(connectedAddress);
-				} catch (Throwable fuckingThrowable){
-					
-				}
-				if(peek != -1){
-					peek = 1;
-				}
+	//Hopefully
+	@Override int dataAvailable() throws SocketException{
+		cli.System.Net.Sockets.Socket fd = getFD();
+		cli.System.Net.Sockets.Socket fd1 = getFD1();
+		int rv = -1;
+		int rv1 = -1;
+		int[] retptr = new int[1];
+		retptr[0] = 0;
+		if (fd != null) {	
+			rv = ioctlsocket(fd, FIONREAD, retptr);
+			int retval = retptr[0];
+			if (retval > 0) {
+				return retval;
 			}
 		}
-		return peek;
+		retptr[0] = 0;
+		if (fd1 != null) {	
+			rv1 = ioctlsocket(fd1, FIONREAD, retptr);
+			int retval = retptr[0];
+			if (retval > 0) {
+				return retval;
+			}
+		}
+		
 	}
 }
